b2b369b72b595c1b4c62acce5ce4f785b1702e09
Julianna Vaccaro <julianna.vicente@icloud.com>
2026-07-31 20:12:22 -0400

    Added POST request function

--- scripts/api.js
+++ scripts/api.js
@@ -11,3 +11,25 @@ export async function getPosts(){
     }
 }
 
+export async function createPost(title, body){
+    try{
+        const response = await fetch("https://jsonplaceholder.typicode.com/posts",{
+            method: "POST",
+            body: JSON.stringify({
+            title: title,
+            body: body,
+            userId: 1
+
+        }),
+        headers:{
+            "Content-type": "application/json; charset=UTF-8"
+        }
+    });
+    const data = await response.json();
+
+    return data;
+
+} catch (error){
+    console.log(error);
+}
+}
